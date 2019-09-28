457c3b7566d521ecaa8c1d41bbefb698f43e25fa
Fedor Chegarovsky <fedor.chegarovsky@ringcentral.com>
2019-09-28 18:54:56 +0300

    refactor: VideoFrame custom deleter refactored
    the deleter replaced with a lambda,
    unwrapCallback turned into a static member of VideoFramesController

--- shell/common/api/custom_media_stream.h
+++ shell/common/api/custom_media_stream.h
@@ -43,6 +43,19 @@ class VideoFramesController {
  public:
   using Format = VideoFrame::Format;
 
+  // Retrieves VideoFramesController pointer from an internal field of an object
+  static VideoFramesController* unwrap(v8::Local<v8::Value> value) {
+    if (!value->IsObject())
+      return nullptr;
+
+    auto obj = v8::Local<v8::Object>::Cast(value);
+    if (obj->InternalFieldCount() != 2)
+      return nullptr;
+
+    void* ptr = obj->GetAlignedPointerFromInternalField(1);
+    return static_cast<VideoFramesController*>(ptr);
+  }
+
   // Allocates a frame of the specific format
   // timestamp is in milliseconds
   virtual VideoFrame* allocateFrame(double timestamp,
@@ -56,49 +69,6 @@ class VideoFramesController {
   virtual void releaseFrame(VideoFrame* frame) = 0;
 };
 
-// Retrieves VideoFramesController pointer from an internal field of an object
-// TODO: Why inline? move implementation to .cc
-inline VideoFramesController* unwrapCallback(v8::Local<v8::Value> value) {
-  if (!value->IsObject())
-    return nullptr;
-  v8::Local<v8::Object> obj = v8::Local<v8::Object>::Cast(value);
-  if (obj->InternalFieldCount() != 2)
-    return nullptr;
-  return static_cast<VideoFramesController*>(
-      obj->GetAlignedPointerFromInternalField(1));
-}
-
-struct VideoFrameCallbackHolder;
-
-namespace detail {
-
-using VideoFrameCallbackHolderPtr = std::shared_ptr<VideoFrameCallbackHolder>;
-
-// Custom deleter for VideoFrame
-// TODO: Refactor to a class, groom, rename to VideoFrameDeleter
-// TODO: Rename callback_ to holder_
-struct VideoFrameReleaser {
-  void operator()(VideoFrame* frame) const;
-  // private:
-  friend VideoFrameCallbackHolder;
-  explicit VideoFrameReleaser(VideoFrameCallbackHolderPtr);
-  VideoFrameReleaser(VideoFrameReleaser const&);
-  ~VideoFrameReleaser();
-
-  // Controller holder
-  VideoFrameCallbackHolderPtr callback_;
-};
-
-// TODO: Why inline? move implementation to .cc
-// TODO: Before moving to .cc - check that it will compile in the C++ project
-inline VideoFrameReleaser::VideoFrameReleaser(VideoFrameCallbackHolderPtr cb)
-    : callback_(cb) {}
-inline VideoFrameReleaser::VideoFrameReleaser(VideoFrameReleaser const&) =
-    default;
-inline VideoFrameReleaser::~VideoFrameReleaser() = default;
-
-}  // namespace detail
-
 // Helper object that wraps VideoFramesController functionality
 // Simplifies allocation and deletion of non-GC frames
 // in a safe manner.
@@ -108,13 +78,18 @@ inline VideoFrameReleaser::~VideoFrameReleaser() = default;
 struct VideoFrameCallbackHolder
     : std::enable_shared_from_this<VideoFrameCallbackHolder> {
   using Format = VideoFrame::Format;
-  using FramePtr = std::unique_ptr<VideoFrame, detail::VideoFrameReleaser>;
+  using FramePtr =
+      std::unique_ptr<VideoFrame, std::function<void(VideoFrame*)>>;
 
   // Allocates a frame with a specific format
   // timestamp is in milliseconds
   FramePtr allocate(double timestamp, const Format* format) {
-    return {callback_->allocateFrame(timestamp, format),
-            detail::VideoFrameReleaser(shared_from_this())};
+    auto sharedThis = shared_from_this();
+    auto frameDeleter = [sharedThis](VideoFrame* f) {
+      sharedThis->callback_->releaseFrame(f);
+    };
+
+    return FramePtr(callback_->allocateFrame(timestamp, format), frameDeleter);
   }
   // TODO: Move implementations to .cc
   FramePtr allocate(double timestamp, const Format& f) {
@@ -133,7 +108,7 @@ struct VideoFrameCallbackHolder
   static std::shared_ptr<VideoFrameCallbackHolder> unwrap(
       v8::Isolate* isolate,
       v8::Local<v8::Value> value) {
-    auto* cb = unwrapCallback(value);
+    auto* cb = VideoFramesController::unwrap(value);
     if (cb) {
       return std::make_shared<VideoFrameCallbackHolder>(
           isolate, v8::Local<v8::Object>::Cast(value), cb);
@@ -147,9 +122,6 @@ struct VideoFrameCallbackHolder
   ~VideoFrameCallbackHolder();
 
  private:
-  // TODO: Remove friend, make accessors
-  friend detail::VideoFrameReleaser;
-
   // Controller wrapper
   v8::Global<v8::Object> wrapper_;
 
@@ -165,13 +137,6 @@ inline VideoFrameCallbackHolder::VideoFrameCallbackHolder(
     : wrapper_(isolate, wrapper), callback_(cb) {}
 
 inline VideoFrameCallbackHolder::~VideoFrameCallbackHolder() = default;
-
-namespace detail {
-// TODO: WHy inline? move to .cc
-inline void VideoFrameReleaser::operator()(VideoFrame* frame) const {
-  callback_->callback_->releaseFrame(frame);
-}
-}  // namespace detail
 
 }  // namespace CustomMediaStream
 
